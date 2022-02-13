700c227d5dec92dda89bb85aa1eaa83c4edcc12e
Ruiz <1436385860@qq.com>
2022-02-13 17:03:28 +0800

    简述：社区模块开发

--- app/src/main/java/com/gdlgxy/internshipcommunity/MainActivity.java
+++ app/src/main/java/com/gdlgxy/internshipcommunity/MainActivity.java
@@ -1,9 +1,12 @@
 package com.gdlgxy.internshipcommunity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 
+import com.gdlgxy.internshipcommunity.base.BaseActivity;
+import com.gdlgxy.internshipcommunity.base.IViewBinding;
 import com.gdlgxy.internshipcommunity.module.mainpageconfig.NavGraphBuilder;
 import com.gdlgxy.internshipcommunity.module.mainpageconfig.view.AppBottomBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -17,47 +20,39 @@ import androidx.navigation.fragment.NavHostFragment;
 
 import com.gdlgxy.internshipcommunity.databinding.ActivityMainBinding;
 
-public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
-
-    private ActivityMainBinding binding;
-    private NavController navController;
-    private AppBottomBar navView;
+public class MainActivity extends BaseActivity<ActivityMainBinding>
+        implements BottomNavigationView.OnNavigationItemSelectedListener {
+    private NavController mNavController;
+    private AppBottomBar mNavView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        navView = findViewById(binding.navView.getId());
-
-        Fragment fragment = getSupportFragmentManager().findFragmentById(binding.navHostFragmentActivityMain.getId());
-        navController = NavHostFragment.findNavController(fragment);
-        NavGraphBuilder.build(this,navController,fragment.getChildFragmentManager(),fragment.getId());
-
-        navView.setOnNavigationItemSelectedListener(this);
+        setContentView(mViewBinding.getRoot());
+        mNavView = mViewBinding.navView;
+        Fragment fragment = getSupportFragmentManager().findFragmentById(mViewBinding.navHostFragmentActivityMain.getId());
+        mNavController = NavHostFragment.findNavController(fragment);
+        NavGraphBuilder.build(this, mNavController, fragment.getChildFragmentManager(), fragment.getId());
+        mNavView.setOnNavigationItemSelectedListener(this);
     }
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        navController.navigate(item.getItemId());
+        mNavController.navigate(item.getItemId());
         return true;
     }
 
     @Override
     public void onBackPressed() {
         //当前正在显示的页面destinationId
-        int currentPageId = navController.getCurrentDestination().getId();
-
+        int currentPageId = mNavController.getCurrentDestination().getId();
         //APP页面路导航结构图  首页的destinationId
-        int homeDestId = navController.getGraph().getStartDestination();
-
+        int homeDestId = mNavController.getGraph().getStartDestination();
         //如果当前正在显示的页面不是首页，而我们点击了返回键，则拦截。
         if (currentPageId != homeDestId) {
-            navView.setSelectedItemId(homeDestId);
+            mNavView.setSelectedItemId(homeDestId);
             return;
         }
-
         //否则 finish，此处不宜调用onBackPressed。因为navigation会操作回退栈,切换到之前显示的页面。
         finish();
     }
@@ -65,5 +60,15 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     @Override
     public boolean dispatchTouchEvent(MotionEvent ev) {
         return super.dispatchTouchEvent(ev);
+    }
+
+    @Override
+    public String getPageName() {
+        return null;
+    }
+
+    @Override
+    public ActivityMainBinding inflate_Activity(LayoutInflater layoutInflater) {
+        return ActivityMainBinding.inflate(layoutInflater);
     }
 }
--- app/src/main/java/com/gdlgxy/internshipcommunity/module/home/HomeFragment.java
+++ app/src/main/java/com/gdlgxy/internshipcommunity/module/home/HomeFragment.java
@@ -1,1 +1,61 @@
-package com.gdlgxy.internshipcommunity.module.home;import android.os.Bundle;import android.util.Log;import android.view.LayoutInflater;import android.view.View;import android.view.ViewGroup;import android.widget.TextView;import com.gdlgxy.internshipcommunity.base.BaseFragment;import com.gdlgxy.internshipcommunity.constant.PageName;import com.gdlgxy.internshipcommunity.databinding.FragmentHomeBinding;import com.gdlgxy.navannotationmodule.FragmentDestination;import androidx.annotation.NonNull;import androidx.annotation.Nullable;import androidx.lifecycle.Observer;import androidx.lifecycle.ViewModelProvider;import androidx.viewbinding.ViewBinding;@FragmentDestination(pageUrl = "main/module/home",asStarter = true)public class HomeFragment        extends BaseFragment<FragmentHomeBinding> {    private HomeViewModel homeViewModel;    @Override    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {        super.onViewCreated(view, savedInstanceState);        homeViewModel =                new ViewModelProvider(this).get(HomeViewModel.class);        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {            @Override            public void onChanged(String s) {                viewBinding.text.setText(s);            }        });        Log.d("fragment-onViewCreated", "HomeFragment");    }    @Override    public void onDestroyView() {        super.onDestroyView();        homeViewModel = null;    }    @Override    public FragmentHomeBinding inflate_Fragment(LayoutInflater layoutInflater, ViewGroup container, boolean at) {        return FragmentHomeBinding.inflate(layoutInflater,container,false);    }    @Override    public String getPageName() {        return PageName.HOME;    }}
+package com.gdlgxy.internshipcommunity.module.home;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.gdlgxy.internshipcommunity.base.BaseFragment;
+import com.gdlgxy.internshipcommunity.constant.PageName;
+import com.gdlgxy.internshipcommunity.databinding.FragmentHomeBinding;
+import com.gdlgxy.navannotationmodule.FragmentDestination;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.viewbinding.ViewBinding;
+
+@FragmentDestination(pageUrl = "main/module/home", asStarter = true)
+public class HomeFragment extends BaseFragment<FragmentHomeBinding> {
+    private HomeViewModel mHomeViewModel;
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mHomeViewModel = new ViewModelProvider(this).get(HomeViewModel.class);
+        mHomeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                mViewBinding.text.setText(s);
+            }
+        });
+        Log.d("fragment-onViewCreated", "HomeFragment");
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mHomeViewModel = null;
+    }
+
+    @Override
+    public FragmentHomeBinding inflate_Fragment(LayoutInflater layoutInflater, ViewGroup container, boolean at) {
+        return FragmentHomeBinding.inflate(layoutInflater, container, false);
+    }
+
+    @Override
+    public String getPageName() {
+        return PageName.HOME;
+    }
+
+    public static HomeFragment newInstance(String feedType) {
+        Bundle args = new Bundle();
+        args.putString("feedType", feedType);
+        HomeFragment fragment = new HomeFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+}
