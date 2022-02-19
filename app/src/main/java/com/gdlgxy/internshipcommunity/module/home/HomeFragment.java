153bfac9f3f22623ed949624f4ab3efe385bd5ec
zhangyaohua <1436385860@qq.com>
2022-02-20 00:48:27 +0800

    简述：社区模块开发-5

--- app/src/main/java/com/gdlgxy/internshipcommunity/module/home/HomeFragment.java
+++ app/src/main/java/com/gdlgxy/internshipcommunity/module/home/HomeFragment.java
@@ -7,18 +7,20 @@ import com.gdlgxy.internshipcommunity.base.BaseListFragment;
 import com.gdlgxy.navannotationmodule.FragmentDestination;
 import com.scwang.smartrefresh.layout.api.RefreshLayout;
 
+import java.util.List;
+
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.lifecycle.Observer;
+import androidx.paging.ItemKeyedDataSource;
 import androidx.paging.PagedList;
 import androidx.paging.PagedListAdapter;
 
 @FragmentDestination(pageUrl = "main/module/home", asStarter = true)
 public class HomeFragment extends BaseListFragment<HomeTabData, HomeViewModel> {
     private PageListPlayDetector playDetector;
-    private String feedType;
+    private String mPageType;
     private boolean shouldPause = true;
-    private HomeViewModel mHomeViewModel;
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
@@ -29,17 +31,18 @@ public class HomeFragment extends BaseListFragment<HomeTabData, HomeViewModel> {
                 submitList(feeds);
             }
         });
+        playDetector = new PageListPlayDetector(this, mRecyclerView);
+        mViewModel.setFeedType(mPageType);
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
-        mHomeViewModel = null;
     }
 
     public static HomeFragment newInstance(String feedType) {
         Bundle args = new Bundle();
-        args.putString("feedType", feedType);
+        args.putString("pageType", feedType);
         HomeFragment fragment = new HomeFragment();
         fragment.setArguments(args);
         return fragment;
@@ -47,8 +50,8 @@ public class HomeFragment extends BaseListFragment<HomeTabData, HomeViewModel> {
 
     @Override
     public PagedListAdapter getAdapter() {
-        feedType = getArguments() == null ? "all" : getArguments().getString("feedType");
-        return new HomePagedListAdapter(getContext(), feedType) {
+        mPageType = getArguments() == null ? "all" : getArguments().getString("pageType");
+        return new HomePagedListAdapter(getContext(), mPageType) {
             @Override
             public void onViewAttachedToWindow2(@NonNull ViewHolder holder) {
                 if (holder.isVideoItem()) {
@@ -82,6 +85,31 @@ public class HomeFragment extends BaseListFragment<HomeTabData, HomeViewModel> {
 
     @Override
     public void onLoadMore(@NonNull RefreshLayout refreshLayout) {
+        final PagedList<HomeTabData> currentList = mAdapter.getCurrentList();
+        if (currentList == null || currentList.size() <= 0) {
+            finishRefresh(false);
+            return;
+        }
+        HomeTabData homeTabData = currentList.get(mAdapter.getItemCount() - 1);
+        mViewModel.loadAfter(homeTabData.id, new ItemKeyedDataSource.LoadCallback<HomeTabData>() {
+            @Override
+            public void onResult(@NonNull List<? extends HomeTabData> data) {
+                PagedList.Config config = currentList.getConfig();
+                if (data != null && data.size() > 0) {
+                    //这里 咱们手动接管 分页数据加载的时候 使用MutableItemKeyedDataSource也是可以的。
+                    //由于当且仅当 paging不再帮我们分页的时候，我们才会接管。所以 就不需要ViewModel中创建的DataSource继续工作了，所以使用
+                    //MutablePageKeyedDataSource也是可以的
+                    MutablePageKeyedDataSource dataSource = new MutablePageKeyedDataSource();
+
+                    //这里要把列表上已经显示的先添加到dataSource.data中
+                    //而后把本次分页回来的数据再添加到dataSource.data中
+                    dataSource.data.addAll(currentList);
+                    dataSource.data.addAll(data);
+                    PagedList pagedList = dataSource.buildNewPagedList(config);
+                    submitList(pagedList);
+                }
+            }
+        });
 
     }
 
